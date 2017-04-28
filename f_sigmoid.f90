8de0667648305e331d5c891088a6015de5c43321
Drzamich <drzamich@gmail.com>
2017-04-28 22:45:39 +0200

    2nd commit

--- f_sigmoid.f90
+++ f_sigmoid.f90
@@ -4,3 +4,18 @@ real function sigmoid(x)
 
     sigmoid = 1/(1+exp(-1*(x)))
 end function
+
+
+subroutine matrixSigmoid(matrix,rows,columns)
+    implicit none
+    integer rows, columns
+    real matrix(rows,columns)
+    integer i,j
+    real sigmoid
+
+    do i=1,rows
+        do j=1,columns
+            matrix(i,j) = sigmoid(matrix(i,j))
+        end do
+    end do
+end subroutine
--- main.f90
+++ main.f90
@@ -4,19 +4,55 @@ program main
     integer inputDataColumns, inputLayerCells
     integer inputDataRows, outputDataRows
     integer outputDataColumns, outputLayerCells
+    integer hiddenValuesSize
+    integer inputToHiddenWeightsRows, inputToHiddenWeightsColumns
 
     integer hiddenLayerCells
 
+    real lowerRandomWeightValue,upperRandomWeightValue
     real,allocatable:: inputToHiddenWeights(:,:)
     real,allocatable:: hiddenToOutputWeights(:,:)
+    real,allocatable:: inputValues(:,:)
+    real,allocatable:: hiddenValues(:,:)
 
     real randomFromRange
     real reallyRandom
+    real sigmoid
     integer i
-    real test(10,2)
 
-    call assignRandomWeights(test,10,2,-0.1,0.1)
+    inputDataRows = 3
+    inputDataColumns = 2
+    lowerRandomWeightValue = -0.1
+    upperRandomWeightValue = 0.1
+    hiddenValuesSize = inputDataRows
+    inputToHiddenWeightsRows = inputDataColumns
+    inputToHiddenWeightsColumns = inputDataRows
 
-    call writeMatrix(test,10,2)
+    allocate(inputValues(inputDataRows,inputDataColumns))
 
+    ! TEST VALUES OF INPUT DATA
+    inputValues = reshape((/1.0,2.0,3.0,6.0,7.0,8.0/),shape(inputValues))
+    write(*,*) "Input values"
+    call writeMatrix(inputValues,inputDataRows,inputDataColumns)
+
+    allocate(inputToHiddenWeights(inputToHiddenWeightsRows,inputToHiddenWeightsColumns))
+    allocate(hiddenValues(hiddenValuesSize,hiddenValuesSize))
+
+    call assignRandomWeights(inputToHiddenWeights,&
+                            inputToHiddenWeightsRows,inputToHiddenWeightsColumns,&
+                            lowerRandomWeightValue,upperRandomWeightValue)
+
+    write(*,*)
+    write(*,*) "Weights from input to hidden layer"
+    call writeMatrix(inputToHiddenWeights,inputToHiddenWeightsRows,inputToHiddenWeightsColumns)
+
+    write(*,*)
+    write(*,*) "Input values multiplied by weights"
+    hiddenValues = matmul(inputValues,inputToHiddenWeights)
+    call writeMatrix(hiddenValues,hiddenValuesSize,hiddenValuesSize)
+
+    write(*,*)
+    write(*,*) "Sigmoided values of hidden layer"
+    call matrixSigmoid(hiddenValues,hiddenValuesSize,hiddenValuesSize)
+    call writeMatrix(hiddenValues,hiddenValuesSize,hiddenValuesSize)
 end
