e261571527ec6ca36f2e637ff7ad7a14aa2d6d49
Drzamich <drzamich@gmail.com>
2017-05-12 22:08:24 +0200

    6th

--- plot.plt
+++ plot.plt
@@ -1,8 +1,10 @@
  set title "Cost function"
  set xrange[1:        5000 ]
- set yrange[   1.64490643E-12 :   1.49969822E-02 ]
+ set logscale y
  set xlabel "Step"
- set xtics 100
+ set ylabel "Cost value"
+ set xtics 1000
  set style data lines
- set output plot.png
+ set terminal png
+ set output "plot.png"
  plot "cost.txt" title "Cost"
--- plotCost.f90
+++ plotCost.f90
@@ -4,7 +4,7 @@ subroutine plotCost
 
     open(10,file='cost.txt')
     do i=1,iterationSteps
-        write(10,*) costValues(i)
+            write(10,*) costValues(i)
     end do
     close(10)
 
@@ -12,11 +12,14 @@ subroutine plotCost
     write(10,*) 'set title "Cost function"'
     write(10,*) 'set xrange[1:',iterationSteps,']'
     !write(10,*) 'set yrange[',costValues(iterationSteps),':',costValues(1),']'
-    write(10,*) 'set yrange[',costValues(iterationSteps),':',costValues(1),']'
+    !write(10,*) 'set yrange[',costValues(iterationSteps),':',costValues(1),']'
+    write(10,*) 'set logscale y'
     write(10,*) 'set xlabel "Step"'
-    write(10,*) 'set xtics 100'
+    write(10,*) 'set ylabel "Cost value"'
+    write(10,*) 'set xtics 1000'
     write(10,*) 'set style data lines'
-    write(10,*) 'set output plot.png'
+    write(10,*) 'set terminal png'
+    write(10,*) 'set output "plot.png"'
     write(10,*) 'plot "cost.txt" title "Cost"'
     close(10)
 
